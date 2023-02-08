cf2cc11c92309116e81c90068f230313efead6c6
Digisata <hnaufal123@gmail.com>
2023-02-08 21:54:43 +0700

    setup deployment

--- main.go
+++ main.go
@@ -3,21 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/Digisata/digiutilsapi/spec"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 
 	"github.com/Digisata/digiutilsapi/app"
 	"github.com/Digisata/digiutilsapi/controller"
 	"github.com/go-playground/validator/v10"
-	"github.com/joho/godotenv"
 )
 
 // @title           Digiutilsapi Specification
 // @description     The purpose of this application is to provide an API for handy tools to solve trivial daily life problems.
 // @termsOfService  http://swagger.io/terms/
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("failed to load env")
+	env := os.Getenv("ENV")
+	if env == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	port := os.Getenv("PORT")
